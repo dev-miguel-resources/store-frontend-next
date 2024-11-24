5d346113c31a42c81de7bbd91671969bcd67c9e4
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-11-24 11:58:27 -0300

    working in home page store

---
+++ app/(molecules)/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import '../../../app/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next - Store Auth',
+  description: 'Next Ecommerce Store',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning={true}>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
